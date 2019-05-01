97911904eae233f8c1ce5862e32814739d803bf0
huangzhhui <huangzhwork@gmail.com>
2019-05-02 00:00:54 +0800

    Moved service register logical to service governance component

---
+++ src/ConfigProvider.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://hyperf.io
+ * @document https://doc.hyperf.io
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf-cloud/hyperf/blob/master/LICENSE
+ */
+
+namespace Hyperf\ServiceGovernance;
+
+use Hyperf\ServiceGovernance\Register\ConsulAgent;
+use Hyperf\ServiceGovernance\Register\ConsulAgentFactory;
+
+class ConfigProvider
+{
+    public function __invoke(): array
+    {
+        return [
+            'dependencies' => [
+                ConsulAgent::class => ConsulAgentFactory::class,
+            ],
+            'commands' => [
+            ],
+            'scan' => [
+                'paths' => [
+                    __DIR__,
+                ],
+            ],
+        ];
+    }
+}
